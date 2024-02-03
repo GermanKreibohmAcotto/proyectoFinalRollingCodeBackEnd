8143df627bfdf2a79ede897e033d9066d427b41f
Tomas Bach <tomybach.90@gmail.com>
2024-02-03 19:49:10 -0300

    se trabaja en la integracion

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,18 +6,27 @@ const { createUser, getUsers, getUser, updateUser, deleteUser, loginUser } = req
 
 route.post('/',[
     check('correo', 'El campo correo esta vacio').notEmpty(),
+    check('correo', 'Formato incorrecto').isEmail(),
+    check('correo', 'Min: 8 Max: 50').isLength({min:8, max:50}),
+    check('contrasenia', 'El campo contraseñaa esta vacio').notEmpty(),
+    check('contrasenia', 'Min: 8 Max: 30').isLength({min:8, max:30}),
+], createUser)
+route.post('/login',[
     check('correo', 'El campo correo esta vacio').notEmpty(),
     check('correo', 'Formato incorrecto').isEmail(),
     check('correo', 'Min: 8 Max: 50').isLength({min:8, max:50}),
-    check('contrasenia', 'El campo contrasenia esta vacio').notEmpty(),
-    check('contrasenia', 'Min: 8').isLength({min:8}),
-], createUser)
-route.post('/login', loginUser)
+    check('contrasenia', 'El campo contraseña esta vacio').notEmpty(),
+    check('contrasenia', 'Min: 8 Max: 30').isLength({min:8, max:30}),
+], loginUser)
 route.get('/', getUsers)
 route.get('/:id',[
     check('id', 'Formato incorrecto de ID').isMongoId(),
 ], getUser)
-route.put('/:id', updateUser)
-route.delete('/:id', deleteUser)
+route.put('/:id',[
+    check('id', 'Formato incorrecto de ID').isMongoId(),
+], updateUser)
+route.delete('/:id',[
+    check('id', 'Formato incorrecto de ID').isMongoId(),
+], deleteUser)
 
 module.exports = route
