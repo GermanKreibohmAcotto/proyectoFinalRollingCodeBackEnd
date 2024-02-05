2609b49b1ef61726a1d5322fcb7b65b2eb0e9984
Tomas Bach <tomybach.90@gmail.com>
2024-02-05 17:51:29 -0300

    se trabaja en la integracion

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,15 +3,44 @@ const route = express.Router()
 const { createProduct, getProducts, getOneProduct , updateProduct, deleteProduct } = require('../controllers/product.controllers')
 const auth = require('../middlewars/auth')
 const multer = require ("../middlewars/multer")
+const { check } = require('express-validator')
 
-/*crear*/
-route.post('/', multer.single('imagen'), createProduct)
-/*obtener*/
+route.post('/', [
+    check('titulo', 'El campo titulo esta vacio').notEmpty(),
+    check('codigo', 'El campo codigo esta vacio').notEmpty(),
+    check('precio', 'El campo precio esta vacio').notEmpty(),
+    check('descripcion', 'El campo descripcion esta vacio').notEmpty(),
+    check('imagen', 'El campo imagen esta vacio').notEmpty(),
+    check('titulo',  'Min: 5 Max: 50').isLength({min:5, max:50}),
+    check('codigo',  'Min: 5 Max: 50').isLength({min:5, max:50}),
+    check('precio',  'Min: 2 Max: 50').isLength({min:2, max:50}),
+    check('descripcion',  'Min: 10 Max: 50').isLength({min:10, max:50}),
+    check('imagen',  'Min: 5 Max: 80').isLength({min:5, max:80}),
+    multer.single('imagen'),
+], createProduct)
+
 route.get('/', getProducts) 
-route.get('/:id', getOneProduct) 
-/*Actualizar*/
-route.put('/:id', updateProduct)
-/*Borrar*/
-route.delete('/:id', deleteProduct)
+
+route.get('/:id',[
+    check('id', 'Formato incorrecto de ID').isMongoId(),
+], getOneProduct) 
+
+route.put('/:id',[
+    check('id', 'Formato incorrecto de ID').isMongoId(),
+    check('titulo', 'El campo titulo esta vacio').notEmpty(),
+    check('codigo', 'El campo codigo esta vacio').notEmpty(),
+    check('precio', 'El campo precio esta vacio').notEmpty(),
+    check('descripcion', 'El campo descripcion esta vacio').notEmpty(),
+    check('imagen', 'El campo imagen esta vacio').notEmpty(),
+    check('titulo',  'Min: 5 Max: 50').isLength({min:5, max:50}),
+    check('codigo',  'Min: 5 Max: 50').isLength({min:5, max:50}),
+    check('precio',  'Min: 2 Max: 50').isLength({min:2, max:50}),
+    check('descripcion',  'Min: 10 Max: 50').isLength({min:10, max:50}),
+    check('imagen',  'Min: 5 Max: 80').isLength({min:5, max:80}),
+], updateProduct)
+
+route.delete('/:id',[
+    check('id', 'Formato incorrecto de ID').isMongoId(),
+], deleteProduct)
 
 module.exports = route
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,20 +11,24 @@ route.post('/',[
     check('contrasenia', 'El campo contraseñaa esta vacio').notEmpty(),
     check('contrasenia', 'Min: 8 Max: 30').isLength({min:8, max:30}),
 ], createUser)
-route.post('/login',[
+
+route.post('/login', loginUser)
+
+route.get('/', getUsers)
+
+route.get('/:id',[
+    check('id', 'Formato incorrecto de ID').isMongoId(),
+], getUser)
+
+route.put('/:id',[
+    check('id', 'Formato incorrecto de ID').isMongoId(),
     check('correo', 'El campo correo esta vacio').notEmpty(),
     check('correo', 'Formato incorrecto').isEmail(),
     check('correo', 'Min: 8 Max: 50').isLength({min:8, max:50}),
     check('contrasenia', 'El campo contraseña esta vacio').notEmpty(),
     check('contrasenia', 'Min: 8 Max: 30').isLength({min:8, max:30}),
-], loginUser)
-route.get('/', getUsers)
-route.get('/:id',[
-    check('id', 'Formato incorrecto de ID').isMongoId(),
-], getUser)
-route.put('/:id',[
-    check('id', 'Formato incorrecto de ID').isMongoId(),
 ], updateUser)
+
 route.delete('/:id',[
     check('id', 'Formato incorrecto de ID').isMongoId(),
 ], deleteUser)
