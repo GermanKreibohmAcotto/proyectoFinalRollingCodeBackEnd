e46e184f1119bdca3502e77187a49b3d46e7dfca
Germán Kreibohm Acotto <germanka2003@gmail.com>
2025-02-18 14:45:26 -0300

    Solucion de CORS

--- server/app.js
+++ server/app.js
@@ -11,7 +11,11 @@ class Server {
 middlewars(){
     this.app.use(express.json())
     this.app.use(morgan('dev'))
-    this.app.use(cors())
+    this.app.use(cors({
+        origin: 'https://proyecto-final-rolling-code-nu.vercel.app', // Origen de tu frontend
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+        allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos
+    }))
 }
 
     routes() {
